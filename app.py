081cfa6d2a1fe3b4127347d9cde301b8e55a3342
Hamid00Fathi <166663270+Hamid00Fathi@users.noreply.github.com>
2026-09-06 15:52:38 +0330

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://gamenet-web.onrender.com"}})
+
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 db = {}   # ذخیرهٔ وضعیت کاربران
